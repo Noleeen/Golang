75b5ab09c6b7cea9c6dcebbffb3f3bd6d6bca07f
Ivan Gerasimenko <noleen@ya.ru>
2023-01-04 13:21:35 +0300

    function2

--- main.go
+++ main.go
@@ -5,18 +5,34 @@ import (
 	"fmt"
 )
 
+// функция init запускается при инициализации пакета перед main
+//var msg string
+//func init() {
+//	msg = "what do you want?"
+//}
+
 func main() {
+	f := anon()
+	g := anon()
+
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(g())
+	fmt.Println(anon())
+	fmt.Println(prediction("wednesday"))
+	fmt.Println(findMax(1, 153, 99, 1111))
+
 	message, err := enterTheClub(15)
 	if err != nil {
-		//log.Fatal(err)
+		//	//log.Fatal(err)
 		fmt.Println(err)
 		return
 	}
 	fmt.Println(message)
+
 }
 
 func print(m string) {
-
 	fmt.Println("функция принт пишет ", m)
 }
 
@@ -33,4 +49,45 @@ func enterTheClub(age int) (string, error) {
 		return "на баре забери приз", nil
 	}
 	return "ты ещё не дорос", errors.New("you are to young")
+}
+
+// конструкция switch case
+func prediction(dayOfWeek string) (string, error) {
+	switch dayOfWeek {
+	case "monday":
+		return "you will find good job", nil
+	case "tuesday":
+		return "you will do it", nil
+	case "wednesday":
+		return "you will have great dinner", nil
+	case "thursday":
+		return "this day is fishDay", nil
+	case "friday":
+		return "ops...", nil
+	default:
+		return "недействительный день недели", errors.New("invalid day of the week")
+	}
+}
+
+// итерация в цикле
+func findMax(numbers ...int) int {
+	if len(numbers) == 0 {
+		return 0
+	}
+	max := numbers[0]
+	for _, i := range numbers {
+		if i > max {
+			max = i
+		}
+	}
+	return max
+}
+
+// анонимная функция с замыканкием
+func anon() func() int {
+	x := 0
+	return func() int {
+		x++
+		return x
+	}
 }
