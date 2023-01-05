3a9010457c181e77757508af960ad08fb15cba4d
Ivan Gerasimenko <noleen@ya.ru>
2023-01-05 13:44:11 +0300

    указатели, массивы, слайсы, матрица, мапы, структуры, конструктор

--- example/main2.go
+++ example/main2.go
@@ -2,31 +2,89 @@ package main
 
 import "fmt"
 
-// МАПЫ-----------------------------------------------------
-func main() {
-	users := map[string]int{
-		"Nina":  17,
-		"Sasha": 15,
-		"Petya": 21,
-		"Lesya": 18,
-		"Aryna": 10,
-	}
-
-	age, exists := users["Sasha"]
-
-	if exists {
-		fmt.Println(exists, age, "\n")
-	} else {
-		fmt.Println("this name isn't in map\n")
-	}
-
-	users["Kate"] = 27     // добавляем новый ключ и значение в мапу
-	delete(users, "Petya") // удаление записи по ключу
-	// ИТЕРАЦИЯ ПО МАПАМ
-	for key, value := range users {
-		fmt.Println(key, value)
+// КОНСТРУКТОР---------------------------------------------
+// то же самое как и в нижней структуре:
+func newUser(name, sex string, age, weight, height int) User {
+	return User{
+		name:   name,
+		age:    age,
+		sex:    sex,
+		weight: weight,
+		height: height,
 	}
 }
+func main() {
+	user1 := newUser("Vasya", "Male", 72, 82, 176)
+}
+
+// ЕЩЁ ОДИН ВИД ОБЪЯВЛЕНИЯ КОНСТРУКТОРА
+type DumbDatabase struct {
+	m map[string]string
+}
+
+func NewDumbDatabase() *DumbDatabase {
+	return &DumbDatabase{
+		m: make(map[string]string),
+	}
+}
+
+//-----------------------------------------------------------------------
+
+// СТРУКТУРЫ -----------------------------------------------
+//	user := struct {
+//		name   string
+//		age    int
+//		sex    string
+//		weight int
+//		height int
+//	}{"Vasya", 72, "Male", 82, 176}   // такой вариант объявления структуры нельзя переиспользовать, поэтому используют чаще другой (СМ. НИЖЕ)
+
+type User struct {
+	name   string
+	age    int
+	sex    string
+	weight int
+	height int
+}
+
+func main() {
+	user1 := User{"Vasya", 72, "Male", 82, 176}
+	user2 := User{"Nina", 65, "Female", 90, 173}
+
+	fmt.Println("\n", user1, "\n")
+	fmt.Println(user1.name, user1.sex, "\n")
+	fmt.Printf("%+v\n", user1)
+	fmt.Printf("%+v\n", user2)
+
+}
+
+// ---------------------------------------------------------
+
+// МАПЫ-----------------------------------------------------
+//func main() {
+//	users := map[string]int{
+//		"Nina":  17,
+//		"Sasha": 15,
+//		"Petya": 21,
+//		"Lesya": 18,
+//		"Aryna": 10,
+//	}
+//
+//	age, exists := users["Sasha"]
+//
+//	if exists {
+//		fmt.Println(exists, age, "\n")
+//	} else {
+//		fmt.Println("this name isn't in map\n")
+//	}
+//
+//	users["Kate"] = 27     // добавляем новый ключ и значение в мапу
+//	delete(users, "Petya") // удаление записи по ключу
+//	// ИТЕРАЦИЯ ПО МАПАМ
+//	for key, value := range users {
+//		fmt.Println(key, value)
+//	}
+//}
 
 //----------------------------------------------------------
 
